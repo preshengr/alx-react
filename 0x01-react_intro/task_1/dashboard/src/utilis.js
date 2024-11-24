7b3ed063da767f401c3be71f86bf5e1203e9b408
Precious Sunday <preshengr@gmail.com>
2024-11-24 04:53:14 +0100

    Updates

--- 0x01-react_intro/task_1/dashboard/src/utilis.js
+++
@@ -1,7 +0,0 @@
-export function getFullYear() {
-    return new Date().getFullYear();
-}
-
-export function getFooterCopy(isIndex) {
-    return isIndex ? 'Holberton school' : 'Holberton School main dashboard';
-}
--- 0x08-react_redux_reducer_selector/README.md
+++
@@ -1,1 +0,0 @@
-# React Redux reducer+selector
